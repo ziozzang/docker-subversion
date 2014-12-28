0a51199c229548f4fb019f332d42cf03ee2d0e80
ron <ron.kirschler@gmail.com>
2014-12-28 17:45:10 +0100

    Initial checkin

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --name subversion -d -p 80:80 -v /srv/subversion/data:/var/svn -v /srv/subversion/conf:/etc/svn rkrx/subversion
+docker run --name subversion -i -p 80:80 -v /srv/subversion/data:/var/svn -v /srv/subversion/conf:/etc/svn rkrx/subversion
