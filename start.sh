5fd3da4768bcb22f47d3b460f53644a8c357bfbb
ron <ron.kirschler@gmail.com>
2014-12-28 16:13:42 +0100

    Initial checkin

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-source /etc/apache2/envvars
-exec apache2 -D FOREGROUND
+#source /etc/apache2/envvars
+#exec apache2 -D FOREGROUND
+/etc/init.d/apache2 start
+tail -f /var/log/apache2/error.log
